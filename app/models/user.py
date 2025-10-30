28caac118388a5226ab8f369fde85a6204728d53
unknown <thduszbzb12@gmail.com>
2025-10-30 14:18:41 +0900

    Fix:model,import경로 수정, validators.py 함수 에러 수정

--- app/models/user.py
+++ app/models/user.py
@@ -6,13 +6,13 @@ from ..extensions import db
 class User(db.Model, UserMixin):
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True) # 기본키, id(숫자)
-  username = db.Column(db.String(30), unique=True, index= True)
-  nickname = db.Column(db.String(30), unique=True, index=True, nullable=True)
+  username = db.Column(db.String(30), unique=True, index= True, nullable=False)
+  nickname = db.Column(db.String(30), unique=True, index=True, nullable=False)
   password_hash = db.Column(db.String(255), nullable=False)
-  email = db.Column(db.String(100), unique=True)
-  created_at = db.Column(db.DateTime, default=datetime.now)
-  updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
-  deleted_at = db.Column(db.DateTime, nullable=True)
+  email = db.Column(db.String(100), unique=True, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+  updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
+  deleted_at = db.Column(db.DateTime, default=None, nullable=True)
 
   def set_password(self, password): # 암호화
     self.password_hash = generate_password_hash(password)
