f553bcf6bc4bf64fe22904edb4560670bd9fe4b8
sysong0516 <sysong777@naver.com>
2025-10-30 15:02:29 +0900

    routine_log routine 간 의존성옵션 추가

--- app/models/routine.py
+++ app/models/routine.py
@@ -7,7 +7,7 @@ class Routine(db.Model):
   __tablename__='routine'
 
   id = db.Column(db.Integer, primary_key=True)
-  drug_id = db.Column(db.Integer, nullable=False) #db.ForeignKey('drug.id')
+  drug_id = db.Column(db.Integer, nullable=False, unique=True) #db.ForeignKey('drug.id')
   author_id = db.Column(db.Integer, nullable=False) #db.ForeignKey('users.id')
   # author=db.relationship('User',backref=db.backref('routine'))
   start_date = db.Column(db.Date, nullable=False)
