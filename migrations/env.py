d1f2bec99fb0e5be4edfaabe9b78316ce75bbf85
yeonhee2 <qodusgml03@naver.com>
2025-10-31 15:30:58 +0900

    feat: drug_contraindications db에서 삭제 안되게 추가함

--- migrations/env.py
+++ migrations/env.py
@@ -50,7 +50,7 @@ def get_metadata():
         return target_db.metadatas[None]
     return target_db.metadata
 
-AUTOMAP_SKIP = {"health_products", "drug_products"}
+AUTOMAP_SKIP = {"health_products", "drug_products", "drug_contraindications"}
 
 def include_object(object, name, type_, reflected, compare_to):
     """
