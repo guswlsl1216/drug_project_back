40a0e0829985641fd74903b3a3bee5ef3db1ee97
guswlsl1216 <guswls023@naver.com>
2025-10-31 16:08:01 +0900

    Feat:mydrug저장기능구현

--- app/models/user_meds.py
+++ app/models/user_meds.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-
-from sqlalchemy import JSON
 from ..extensions import db
+from sqlalchemy import Enum
 import os
 from ..config import Config
 
@@ -13,9 +12,8 @@ class User_meds(db.Model):
   # name => ai가 인식한 name과 유저가 수정한 name 따로 관리 
   predicted_name = db.Column(db.String(255), nullable=True) # 모델이 예측한 약 이름 
   confirmed_name = db.Column(db.String(255), nullable=True) # 유저가 직접 입력한 약 이름 
-
-  item_type = db.Column(db.String(20), nullable=False) # 약 vs 영양제 구분 필드
-  taken = db.Column(db.String(20), nullable=False, default='') # 복용 완료 여부 
+  item_type = db.Column(Enum('medicine', 'supplement'), nullable=False) # 약 vs 영양제 구분 필드
+  taken = db.Column(db.Boolean, nullable=False, default=False)  # 복용 완료 여부 
   created_at = db.Column(db.DateTime, default=datetime.now)
   updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
