4874280894a7efee014edb95773a674b96dfd6ec
jjop2 <jngsuh616@gmail.com>
2025-11-04 10:52:11 +0900

    Feat: 분석 결과 모델 추가

--- app/models/analyze_result.py
+++ app/models/analyze_result.py
@@ -7,19 +7,22 @@ class Analyze_result(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   status = db.Column(db.Integer, nullable=False)
-  meds = db.Column(JSON)
-  supp_name = db.Column(db.String(255))
+  meds_ingredients = db.Column(JSON)
   supps = db.Column(JSON)
   analysis_date = db.Column(db.DateTime, default=datetime.now)
+  duplicates = db.Column(JSON)
+  interactions = db.Column(JSON)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  # user = db.relationship('User', backref=db.backref('analyze_results', lazy=True))
 
 
-# def to_dict(self):
-#   return {
-#     'id':self.id,
-#     'status':self.status,
-#     'meds':self.meds,
-#     'supp_name':self.supp_name,
-#     'supps':self.supps,
-#     'analysis_date':self.analysis_date
-#   }
+def to_dict(self):
+  return {
+    'id':self.id,
+    'status':self.status,
+    'meds_ingredients':self.meds_ingredients,
+    'supps':self.supps,
+    'analysis_date':self.analysis_date.strftime('%Y-%m-%d %H:%M:%S'),
+    'duplicates':self.duplicates,
+    'interactions':self.interactions
+  }
--- app/models/analyze_result_detail.py
+++ app/models/analyze_result_detail.py
@@ -4,7 +4,10 @@ from sqlalchemy import JSON
 class Analyze_result_detail(db.Model):
   __tablename__ = 'analyze_result_detail'
 
-  id = db.Column(db.Integer, primary_key=True)
-  analysis_id = db.Column(db.Integer, db.ForeignKey('analyze_result.id'))
+  analysis_id = db.Column(
+    db.Integer,
+    db.ForeignKey('analyze_result.id'),
+    primary_key=True
+  )
   duplicates = db.Column(JSON)
   interactions = db.Column(JSON)
