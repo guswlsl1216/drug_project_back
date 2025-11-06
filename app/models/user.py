ce4f9caa1fcffe49c2e2d397ac19149d90d4a762
unknown <thduszbzb12@gmail.com>
2025-11-06 15:20:19 +0900

    Fix:model point int 추가

--- app/models/user.py
+++ app/models/user.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
   address = db.Column(db.String(200), nullable=True)
   detailed_address = db.Column(db.String(200), nullable=True)
   role = db.Column(db.String(20), nullable=False, default="user")
+  point = db.Column(db.Integer, nullable=True)
 
   def set_password(self, password): # 암호화
     self.password_hash = generate_password_hash(password)
