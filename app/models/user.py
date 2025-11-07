94097071912b9ec4fb02e28dc25b3788d985d37c
yeonhee2 <qodusgml03@naver.com>
2025-11-07 11:39:23 +0900

    feat:쇼핑관련 테이블들 생성

--- app/models/user.py
+++ app/models/user.py
@@ -18,7 +18,11 @@ class User(db.Model, UserMixin):
   address = db.Column(db.String(200), nullable=True)
   detailed_address = db.Column(db.String(200), nullable=True)
   role = db.Column(db.String(20), nullable=False, default="user")
-  point = db.Column(db.Integer, nullable=True)
+  point = db.Column(db.Integer, nullable=True, default=0)
+  goods = db.relationship('Goods', back_populates='user')
+  reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
+  carts = db.relationship('Cart', back_populates='user', cascade='all, delete-orphan')
+  favorites = db.relationship('Favorite', back_populates='user', cascade='all, delete-orphan')
 
   def set_password(self, password): # 암호화
     self.password_hash = generate_password_hash(password)
