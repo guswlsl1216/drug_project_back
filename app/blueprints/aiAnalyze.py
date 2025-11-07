d59e34247d9fd020851640be0b667786d92faa08
ss-oh <tlstmddh12@gmail.com>
2025-11-07 18:03:09 +0900

    1107분석결과기능

--- app/blueprints/aiAnalyze.py
+++ app/blueprints/aiAnalyze.py
@@ -22,19 +22,41 @@ def search_meds():
   
   model = SP if search_type == 'supps' else MP
 
+  if search_type == 'supps':
+    column_name = model.PRDLST_NM
+    #  영양제 모델(SP)의 성분 컬럼을 NUTRITIONAL_FUNCTIONAL_MATERIAL로 가정하고 설정
+    #  만약 이 컬럼명이 다를 경우, 실제 영양제 테이블의 성분 컬럼명으로 변경해야 합니다.
+    ingredient_column = model.RAWMTRL_NM
+  else:
+    column_name = model.ITEM_NAME
+    # 의약품 성분 컬럼: MAIN_INGR_ENG 사용 확정
+    ingredient_column = model.MAIN_INGR_ENG
+
+
   normalized_search_name = search_name.replace(' ', '').lower()
   search_keyword = f"%{normalized_search_name}%"
 
-  column_name = model.PRDLST_NM if search_type == 'supps' else model.ITEM_NAME   
-
   results = (
-    db.session.query(model.id, column_name)
+    db.session.query(model.id, column_name, ingredient_column) 
     .filter(
       func.lower(func.replace(column_name, ' ', '')).like(search_keyword)
     ).all()
   )
 
-  data = [{ "id" : r[0], "name" : r[1]} for r in results]
+  data = []
+  for r in results:
+      item_id, item_name, item_ingredients = r
+      
+      # DB 성분 값이 None일 경우, 프론트엔드의 'undefined' 에러를 막기 위해 빈 문자열로 처리
+      processed_ingredients = item_ingredients if item_ingredients is not None else ""
+      
+      data.append({ 
+          "id" : item_id, 
+          "name" : item_name,
+          "ingredients": processed_ingredients # 성분 값 포함
+      })
+
+
   return jsonify({
         'success': True,
         # 프론트엔드가 'medicines' 키를 예상하므로 여기에 데이터를 담습니다.
