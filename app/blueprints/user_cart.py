e34c191dba46427f332c13e477b0a6a381b53b98
unknown <thduszbzb12@gmail.com>
2025-11-17 19:23:23 +0900

    Fix:장바구니 프론트랑 연동

--- app/blueprints/user_cart.py
+++ app/blueprints/user_cart.py
@@ -1,4 +1,5 @@
 from ..extensions import db
+from sqlalchemy.orm import joinedload
 from ..models.cart import Cart
 from ..models.goods import Goods
 from ..utils.auth_user import get_current_user
@@ -8,24 +9,42 @@ from flask_jwt_extended import jwt_required, current_user
 
 bp = Blueprint('cart',__name__)
 
-user = current_user
-
 # user 장바구니 목록을 불러 옴
 # 수량,합계 추가예정
 @bp.get('/')
 @jwt_required()
 def get_cart():
+  user = current_user
 
   # 장바구니 상품들
-  cart_items = Cart.query.filter_by(user_id=user.id).all()
+  cart_items = (
+    db.session.query(
+      Cart.id.label("cart_id"),
+      Cart.count,
+      Cart.user_id,
+      Goods.id.label("goods_id"),
+      Goods.stock,
+      Goods.goods_name,
+      Goods.image_path,
+      Goods.price
+    )
+    .join(Goods, Cart.goods_id == Goods.id)
+    .filter(Cart.user_id == user.id)
+    .all()
+  )
+  print("="*20,cart_items)
 
   result = []
   for c in cart_items:
     result.append({
-      'id':c.id,
+      'cart_id':c.cart_id,
       'count':c.count,
       'goods_id':c.goods_id,
-      'user_id':c.user_id
+      'goods_name':c.goods_name,
+      'stock':c.stock,
+      'user_id':c.user_id,
+      'image_path':c.image_path,
+      'price':c.price
     })
   
 
@@ -43,18 +62,26 @@ def update_goods(cart_id):
   goods_item = cart_item.goods # Cart 모델의 relationship을 총해 Goods 객체 접근
   stock = goods_item.stock # 해당 상품의 재고를 가져 옴
   
+  print("="*20)
+  print(stock)
+  print(cart_item.count)
+  print(action)
+
   if cart_item and action == 'plus': # 재고 수량 초과 시 더 못 넣게 구현을 해야하는데 그건 프론트에서 해야할지?
-    if stock > cart_item.count :
+    if int(stock) > int(cart_item.count):
 
       cart_item.count += 1
       db.session.commit()
     else:
-      jsonify({'error':'재고 수량을 초과하였습니다.'})
+      return jsonify({'error':'재고 수량을 초과하였습니다.'}),400
       
   elif cart_item and action == 'minus': # 1개 이하로 안 내려가게 수정
-    cart_item.count -= 1
-    db.session.commit()
-
+    if cart_item.count > 1:
+      cart_item.count -= 1
+      db.session.commit()
+    else:
+      return jsonify({'error':'최소 수량은 1개입니다.'}),400
+    
   result = {
     'id':cart_item.id,
     'count':cart_item.count,
@@ -66,6 +93,7 @@ def update_goods(cart_id):
 @bp.post('/<int:goods_id>')
 @jwt_required()
 def post_goods(goods_id):
+  user = current_user
   
   goods = Cart.query.filter_by(user_id=user.id, goods_id=goods_id).first()
   
