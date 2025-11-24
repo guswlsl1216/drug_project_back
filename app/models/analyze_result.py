72d8135b5a5a73d96d70a68aea550d02c0c257a3
jjop2 <jngsuh616@gmail.com>
2025-11-24 13:11:10 +0900

    fix: 분석 결과 내역 이미지 불러오기 수정

--- app/models/analyze_result.py
+++ app/models/analyze_result.py
@@ -15,6 +15,7 @@ class Analyze_result(db.Model):
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
   user = db.relationship('User', backref=db.backref('analyze_results'))
   analysis_uid = db.Column(db.String(64), unique=True, nullable=False)
+  image_url = db.Column(db.String(512))
 
   def to_dict(self):
     return {
@@ -26,5 +27,6 @@ class Analyze_result(db.Model):
       'duplicates':self.duplicates,
       'interactions':self.interactions,
       'user_id':self.user_id,
-      'analysis_uid':self.analysis_uid
+      'analysis_uid':self.analysis_uid,
+      'image_url':self.image_url
     }
